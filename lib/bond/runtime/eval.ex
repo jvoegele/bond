b86b00ac5e88a9428abe6bf11a890c5a623f4fff
Jason Voegele <jason.voegele@gmail.com>
2025-02-11 16:29:34 -0500

    Add Bond.Runtime.Eval module in new runtime subsystem

---
+++ lib/bond/runtime/eval.ex
@@ -0,0 +1,6 @@
+defmodule Bond.Runtime.Eval do
+  @moduledoc internal: true
+  @moduledoc """
+  Internal helper module for runtime execution of contracts and assertions.
+  """
+end
---
+++ test/bond/runtime/eval_test.exs
@@ -0,0 +1,7 @@
+defmodule Bond.Runtime.EvalTest do
+  @moduledoc false
+
+  use ExUnit.Case
+
+  alias Bond.Runtime.Eval
+end
